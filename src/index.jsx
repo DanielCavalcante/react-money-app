3a8dd203317eb5cf2be2d9d68ee656ca119fbadb
Daniel Cavalcante <danielcdesouza@gmail.com>
2018-12-07 00:23:13 -0200

    Remove row

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
-
 import promise from 'redux-promise';
 
 import App from './main/app';
